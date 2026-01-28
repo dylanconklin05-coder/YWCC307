2521eec76758296b3a9f39b7e5826378c8bf7420
Dylan Conklin <dylanConklin05@gmail.com>
2026-01-28 09:08:38 -0500

    did hello world

---
+++ Hello_World/target/debug/Hello_World.d
@@ -0,0 +1,1 @@
+C:\Users\dc7gr\OneDrive\Desktop\YWCC307\YWCC307\Hello_World\target\debug\Hello_World.exe: C:\Users\dc7gr\OneDrive\Desktop\YWCC307\YWCC307\Hello_World\src\main.rs
---
+++ Hello_World/target/debug/deps/Hello_World.d
@@ -0,0 +1,5 @@
+C:\Users\dc7gr\OneDrive\Desktop\YWCC307\YWCC307\Hello_World\target\debug\deps\Hello_World.d: src\main.rs
+
+C:\Users\dc7gr\OneDrive\Desktop\YWCC307\YWCC307\Hello_World\target\debug\deps\Hello_World.exe: src\main.rs
+
+src\main.rs:
